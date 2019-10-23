dac0781d36a8dbebc1759721fde520a5b2fc7278
Joseph Burks <joe_burks_3@yahoo.com>
2019-10-23 18:03:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,17 @@
 def new_hash
-  
+  hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  hash = {
+    key: "value"
+  }
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  hash = {
+  name: 'Grace Hopper'
+  }
 end
 
 def id_generator
